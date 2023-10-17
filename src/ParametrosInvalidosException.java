928c426c695c34158dbbacf566c7a8db1e965188
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-10-16 23:32:25 -0300

    Primeiro commit - subindo projeto

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception{
+
+    public static void main(String[] args) {
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+    }
+}
